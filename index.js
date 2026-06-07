3dac5174ba431885493a55bf86f4abfdde53f141
Ayush <pandaayush25305@gmail.com>
2026-06-07 18:33:10 +0530

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app =express();
 const PORT = process.env.PORT ?? 8000;
 
 app.get('/', (req, res) => {
-    return res.json({message: 'server is healthy',
+    return res.json({message: 'server is healthy v4.1',
         status: 'github Actions',
     });
 });
